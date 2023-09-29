75db5a113ea068880b2ddc1c193b837701d98ad2
switchII <luoandon@gmail.com>
2023-09-29 09:21:16 +0800

    feat:集成sso服务

--- alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/BasePlatformApplication.java
+++ alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/BasePlatformApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.base.platform;
 
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author luoxiaodong
  * @version 1.0.0.
  */
+@EnableInfraSsoApi
 @EnableAsync
 @SpringBootApplication
 @EnableScheduling
