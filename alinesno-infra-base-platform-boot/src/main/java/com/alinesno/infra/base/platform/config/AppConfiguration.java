339fc03484e5af90e99358214567f28e98899866
周健(James) <jamesAip@gmail.com>
2024-08-11 07:59:00 -0400

    新增AppConfiguration类，实现@EnableActable、@EnableInfraSsoApi、@EnableAsync、@MapperScan和@EnableScheduling注解，并配置LogAspect和CommandLineRunner。

---
+++ alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/config/AppConfiguration.java
@@ -0,0 +1,32 @@
+package com.alinesno.infra.base.platform.config;
+
+import com.alinesno.infra.common.facade.enable.EnableActable;
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableActable
+@EnableInfraSsoApi
+@EnableAsync
+@MapperScan("com.alinesno.infra.base.platform.mapper")
+@EnableScheduling
+@Configuration
+public class AppConfiguration implements CommandLineRunner {
+
+    @Bean
+    public LogAspect getLogAspect(){
+        return new LogAspect() ;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
