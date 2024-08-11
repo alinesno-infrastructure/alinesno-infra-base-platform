59a185a8d12f614fdb1f379c64cf312c2ff2445c
周健(James) <jamesAip@gmail.com>
2024-08-11 01:17:00 -0400

    修复未导入的依赖项和多余注释

--- alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/BasePlatformApplication.java
+++ alinesno-infra-base-platform-boot/src/main/java/com/alinesno/infra/base/platform/BasePlatformApplication.java
@@ -1,35 +1,18 @@
 package com.alinesno.infra.base.platform;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import com.alinesno.infra.common.web.log.aspect.LogAspect;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 集成一个Java开发示例工具
  * @author luoxiaodong
  * @version 1.0.0.
  */
-@EnableActable
-@EnableInfraSsoApi
-@EnableAsync
-@MapperScan("com.alinesno.infra.base.platform.mapper")
 @SpringBootApplication
-@EnableScheduling
 public class BasePlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasePlatformApplication.class, args);
-	}
-
-	@Bean
-	public LogAspect getLogAspect(){
-		return new LogAspect() ;
 	}
 
 }
