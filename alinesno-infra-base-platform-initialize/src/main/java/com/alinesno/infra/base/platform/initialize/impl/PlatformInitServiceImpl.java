1bf09831da11263230d82160993966ed614338ec
周健(James) <jamesAip@gmail.com>
2024-08-10 20:43:00 -0400

    新增平台初始化服务实现类PlatformInitServiceImpl。

---
+++ alinesno-infra-base-platform-initialize/src/main/java/com/alinesno/infra/base/platform/initialize/impl/PlatformInitServiceImpl.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.base.platform.initialize.impl;
+
+import com.alinesno.infra.base.platform.initialize.IPlatformInitService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 平台初始化服务实现类
+ */
+@Slf4j
+@Service
+public class PlatformInitServiceImpl implements IPlatformInitService {
+    @Override
+    public void initProductType() {
+
+    }
+
+    @Override
+    public void initProduct() {
+
+    }
+
+    @Override
+    public void initPlanType() {
+
+    }
+
+    @Override
+    public void initPlan() {
+
+    }
+
+    @Override
+    public void initNotice() {
+
+    }
+
+    @Override
+    public void initFriendLink() {
+
+    }
+}
