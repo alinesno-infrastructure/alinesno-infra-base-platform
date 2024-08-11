0e917b2635095200a5ca55dbe1968a36e6992ccb
周健(James) <jamesAip@gmail.com>
2024-08-11 09:58:00 -0400

    实现平台初始化服务接口IPlatformInitService。

---
+++ alinesno-infra-base-platform-initialize/src/main/java/com/alinesno/infra/base/platform/initialize/IPlatformInitService.java
@@ -0,0 +1,38 @@
+package com.alinesno.infra.base.platform.initialize;
+
+/**
+ * 平台初始化服务接口
+ */
+public interface IPlatformInitService {
+
+    /**
+     * 初始化产品类型
+     */
+    void initProductType() ;
+
+    /**
+     * 初始化产品
+     */
+    void initProduct() ;
+
+    /**
+     * 初始化方案类型
+     */
+    void initPlanType() ;
+
+    /**
+     * 初始化方案
+     */
+    void initPlan() ;
+
+    /**
+     * 初始化公告
+     */
+    void initNotice() ;
+
+    /**
+     * 初始化友情链接
+     */
+    void initFriendLink() ;
+
+}
