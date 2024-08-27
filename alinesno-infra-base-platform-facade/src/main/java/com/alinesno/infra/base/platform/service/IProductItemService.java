b43a3a90f9b388feed593d868f67b615c29e4dd6
李娜(Allen) <allenAip@gmail.com>
2024-08-27 05:48:00 -0400

    fix: 更新IProductItemService接口，添加保存产品信息和按类型编码查询产品信息的方法。

--- alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductItemService.java
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IProductItemService.java
@@ -1,8 +1,10 @@
 package com.alinesno.infra.base.platform.service;
 
-import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.api.dto.ProductItemDto;
 import com.alinesno.infra.base.platform.entity.ProductItemEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+
+import java.util.List;
 
 /**
  *
@@ -11,4 +13,17 @@ import com.alinesno.infra.common.facade.services.IBaseService;
  * @version 1.0.0
  */
 public interface IProductItemService extends IBaseService<ProductItemEntity> {
+
+    /**
+     * 保存产品信息
+     * @param productItemDto
+     */
+    void saveOrgProduct(ProductItemDto productItemDto);
+
+    /**
+     * 根据类型编码查询产品信息
+     * @param typeCode
+     * @return
+     */
+    List<ProductItemDto> queryProductByTypeCode(String typeCode);
 }
