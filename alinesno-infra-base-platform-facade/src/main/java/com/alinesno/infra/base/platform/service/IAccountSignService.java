b8b752febe778f8f8353fc8e8aa890b0b1653d18
李娜(Allen) <allenAip@gmail.com>
2024-08-27 06:32:00 -0400

    fix: 更新IAccountSignService接口方法签名

--- alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAccountSignService.java
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IAccountSignService.java
@@ -1,6 +1,5 @@
 package com.alinesno.infra.base.platform.service;
 
-import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
 import com.alinesno.infra.base.platform.entity.AccountSignEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
 
@@ -11,4 +10,27 @@ import com.alinesno.infra.common.facade.services.IBaseService;
  * @version 1.0.0
  */
 public interface IAccountSignService extends IBaseService<AccountSignEntity> {
+
+    /**
+     * 签到
+     *
+     * @param accountId
+     * @return
+     */
+    int signIn(long accountId);
+
+    /**
+     * 查询用户今天是否签到
+     * @param accountId
+     * @return
+     */
+    boolean isSignIn(long accountId);
+
+    /**
+     * 签到后返回用户签到天数
+     * @param accountId
+     * @return
+     */
+    String sayHello(long accountId);
+
 }
