9965c7cfa09f5f55c48137fab6f8f29d6cc88831
李娜(Allen) <allenAip@gmail.com>
2024-08-26 20:51:00 -0400

    优化IContentService接口，新增获取内容列表、详情及更新查看次数的方法。

--- alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IContentService.java
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/IContentService.java
@@ -1,8 +1,10 @@
 package com.alinesno.infra.base.platform.service;
 
-import com.alinesno.infra.base.platform.entity.AccountAuditEntity;
+import com.alinesno.infra.base.platform.api.dto.ContentDto;
 import com.alinesno.infra.base.platform.entity.ContentEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+
+import java.util.List;
 
 /**
  *
@@ -11,4 +13,25 @@ import com.alinesno.infra.common.facade.services.IBaseService;
  * @version 1.0.0
  */
 public interface IContentService extends IBaseService<ContentEntity> {
+
+    /**
+     * 根据类型获取内容列表
+     * @param type
+     * @return
+     */
+    List<ContentDto> getContentListByType(String type);
+
+    /**
+     * 获取内容详情
+     * @param id
+     * @return
+     */
+    ContentDto getContentDetail(long id);
+
+    /**
+     * 更新内容查看次数
+     *
+     * @param id
+     */
+    void updateViewCount(long id);
 }
