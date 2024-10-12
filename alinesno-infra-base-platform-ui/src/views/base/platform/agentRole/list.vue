48189d9959e1b48dbb842366aa4039ec409813b1
王明(AllenQin) <allenQin@gmail.com>
2024-10-12 16:15:49 +0800

    优化列表视图组件，调整搜索条件并添加角色图标及职责展示。

--- alinesno-infra-base-platform-ui/src/views/base/platform/agentRole/list.vue
+++ alinesno-infra-base-platform-ui/src/views/base/platform/agentRole/list.vue
@@ -4,11 +4,11 @@
          <!--应用数据-->
          <el-col :span="24" :xs="24">
             <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
-               <el-form-item label="账户名称" prop="accountName">
-                  <el-input v-model="queryParams.accountName" placeholder="请输入账户名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               <el-form-item label="账户名称" prop="roleName">
+                  <el-input v-model="queryParams.roleName" placeholder="请输入账户名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
                </el-form-item>
-               <el-form-item label="产品名称" prop="productItemName">
-                  <el-input v-model="queryParams['condition[productItemName|like]']" placeholder="请输入产品名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               <el-form-item label="产品名称" prop="roleLink">
+                  <el-input v-model="queryParams['condition[roleLink|like]']" placeholder="请输入产品名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
                </el-form-item>
                <el-form-item>
                   <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -34,8 +34,28 @@
             <el-table v-loading="loading" :data="AgentRoleList" @selection-change="handleSelectionChange">
                <el-table-column type="selection" width="50" align="center" />
                <!-- 业务字段-->
-               <el-table-column label="账户名称" align="center" key="accountName" prop="accountName" v-if="columns[0].visible" />
-               <el-table-column label="产品名称" align="center" key="productItemName" prop="productItemName" v-if="columns[1].visible" :show-overflow-tooltip="true" />
+               <el-table-column label="图标" align="center" width="80" key="status" v-if="columns[2].visible">
+                 <template #default="scope">
+                  <img :src="'http://data.linesno.com/icons/sepcialist/dataset_' + (scope.$index + 1)+ '.png'" style="width:45px;height:45px;border-radius: 5px" />
+                 </template>
+               </el-table-column>
+               <el-table-column label="名称" align="left" key="roleName" prop="roleName" v-if="columns[1].visible" :show-overflow-tooltip="true">
+                  <template #default="scope">
+                     <div>
+                        {{ scope.row.roleName }}
+                     </div>
+                     <div style="font-size: 13px;color: #a5a5a5;cursor: pointer;" v-copyText="scope.row.promptId">
+                        {{ scope.row.responsibilities }}
+                     </div>
+                  </template>
+               </el-table-column>
+               <el-table-column label="链接" align="center" width="150" key="typeOwner" prop="typeOwner">
+                  <template #default="scope">
+                     <el-link type="primary" :href="scope.row.roleLink" target="_blank">
+                        <i class="fa-solid fa-link"></i>&nbsp;访问链接
+                     </el-link>
+                  </template>
+               </el-table-column>
                <el-table-column label="添加时间" align="center" prop="addTime" v-if="columns[2].visible" width="160">
                   <template #default="scope">
                      <span>{{ parseTime(scope.row.addTime) }}</span>
@@ -43,7 +63,7 @@
                </el-table-column>
 
                <!-- 操作字段  -->
-               <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width">
+               <el-table-column label="操作" align="center" width="100" class-name="small-padding fixed-width">
                   <template #default="scope">
                      <el-tooltip content="修改" placement="top" v-if="scope.row.id !== 1">
                         <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
@@ -66,25 +86,25 @@
          <el-form :model="form" :rules="rules" ref="AgentRoleRef" label-width="100px">
             <el-row>
                <el-col :span="24">
-                  <el-form-item label="账户id" prop="accountId">
-                     <el-input v-model="form.accountId"  type="number" placeholder="请输入账户id" maxlength="50" />
+                  <el-form-item label="头像" prop="roleAvatar">
+                     <el-input v-model="form.roleAvatar" placeholder="请输入账户id" maxlength="50" />
                   </el-form-item>
                </el-col>
               <el-col :span="24">
-                <el-form-item label="账户名称" prop="accountName">
-                  <el-input v-model="form.accountName" placeholder="请输入账户名称" maxlength="50" />
+                <el-form-item label="名称" prop="roleName">
+                  <el-input v-model="form.roleName" placeholder="请输入账户名称" maxlength="50" />
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row>
               <el-col :span="24">
-                <el-form-item label="产品id" prop="productItemId">
-                  <el-input v-model="form.productItemId" type="number"   placeholder="请选择产品id" maxlength="255" />
+                <el-form-item label="描述" prop="responsibilities">
+                  <el-input v-model="form.responsibilities"  placeholder="请选择产品id" maxlength="255" />
                 </el-form-item>
               </el-col>
                <el-col :span="24">
-                  <el-form-item label="产品名称" prop="productItemName">
-                     <el-input v-model="form.productItemName"   placeholder="请选择产品名称" maxlength="255" />
+                  <el-form-item label="链接" prop="roleLink">
+                     <el-input v-model="form.roleLink"   placeholder="请选择产品名称" maxlength="255" />
                   </el-form-item>
                </el-col>
             </el-row>
@@ -142,12 +162,12 @@ const data = reactive({
    queryParams: {
       pageNum: 1,
       pageSize: 10,
-      accountId: undefined,
-      productItemId: undefined
+      roleAvatar: undefined,
+      responsibilities: undefined
    },
    rules: {
-      accountId: [{ required: true, message: "名称不能为空", trigger: "blur" }] ,
-      productItemId: [{ required: true, message: "连接不能为空", trigger: "blur" }],
+      roleAvatar: [{ required: true, message: "名称不能为空", trigger: "blur" }] ,
+      responsibilities: [{ required: true, message: "连接不能为空", trigger: "blur" }],
    }
 });
 
@@ -197,10 +217,10 @@ function handleSelectionChange(selection) {
 function reset() {
    form.value = {
       id: undefined,
-      accountId: undefined,
-      accountName: undefined,
-      productItemId: undefined,
-      productItemName: undefined,
+      roleAvatar: undefined,
+      roleName: undefined,
+      responsibilities: undefined,
+      roleLink: undefined,
    };
    proxy.resetForm("AgentRoleRef");
 };
