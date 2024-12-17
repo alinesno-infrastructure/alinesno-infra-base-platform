ff013389dbb3a09ce3c3a758e0c0d9525af51636
陈伟(Victor) <victorAip@gmail.com>
2024-12-18 00:12:06 +0800

    feat: 添加数据权限注解并优化产品项控制器方法。

--- alinesno-infra-base-platform-gateway/src/main/java/com/alinesno/infra/base/platform/gateway/controller/ProductItemController.java
+++ alinesno-infra-base-platform-gateway/src/main/java/com/alinesno/infra/base/platform/gateway/controller/ProductItemController.java
@@ -3,8 +3,11 @@ package com.alinesno.infra.base.platform.gateway.controller;
 import com.alinesno.infra.base.platform.entity.ProductItemEntity;
 import com.alinesno.infra.base.platform.service.IProductItemService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +16,7 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 处理与ProductItemEntity相关的请求的Controller。
@@ -43,11 +43,18 @@ public class ProductItemController extends BaseController<ProductItemEntity, IPr
      * @param page    DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
         return this.toPage(model, this.getFeign(), page);
+    }
+
+    @DataPermissionSave
+    @Override
+    public AjaxResult save(Model model, @RequestBody ProductItemEntity entity) throws Exception {
+        return super.save(model, entity);
     }
 
     @Override
