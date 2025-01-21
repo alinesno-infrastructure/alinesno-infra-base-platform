4b10ba6a055344646b53bc7b32129c1d77eaaf9c
王磊(Levi) <leviAip@gmail.com>
2025-01-22 00:10:49 +0800

    feat:在ISolutionManageService接口中新增获取最新解决方案的方法

--- alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/ISolutionManageService.java
+++ alinesno-infra-base-platform-facade/src/main/java/com/alinesno/infra/base/platform/service/ISolutionManageService.java
@@ -1,7 +1,10 @@
 package com.alinesno.infra.base.platform.service;
 
+import com.alinesno.infra.base.platform.api.dto.SolutionDto;
 import com.alinesno.infra.base.platform.entity.SolutionManageEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+
+import java.util.List;
 
 /**
  *
@@ -10,4 +13,12 @@ import com.alinesno.infra.common.facade.services.IBaseService;
  * @version 1.0.0
  */
 public interface ISolutionManageService extends IBaseService<SolutionManageEntity> {
+
+    /**
+     * 获取最新的解决方案
+     * @param count
+     * @return
+     */
+    List<SolutionDto> getNewSolution(int count);
+
 }
